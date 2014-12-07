e483678d800c23634fcbc3d84f0f5262e8fcbcaa
arainfor@gmail.com <w6n7fIiP8gtU>
2014-12-07 13:58:28 -0500

    Add support for a file to provide the temperature.  Needed so user can set the target.

--- models/thermometer.js
+++ models/thermometer.js
@@ -1,4 +1,5 @@
 var sensor = require('ds18x20');
+var fs = require('fs');
 
 exports.init = function() {
     var isLoaded = sensor.isDriverLoaded();
@@ -18,7 +19,20 @@ exports.init = function() {
  * Get the Thermometer Object.
  */
 exports.getByDeviceId = function(deviceId) {
-    // read the device at path to get the temperature value
     console.log("Reading device: " + deviceId);
+    var pos = deviceId.indexOf("/");
+    if (pos > 0) {
+        // The temperature is a raw double value in a file
+        fs.readFile(deviceId, {encoding: 'utf-8'}, function(err, data){
+            if (!err) {
+                console.log("readFile: " + data);
+                return data;
+            } else {
+                console.log(err);
+            }
+        }
+    }
+
+
     return sensor.get(deviceId);
 };
