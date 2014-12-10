d85f08a1f1b18eb0a583303a892920545fccfd96
arainfor@gmail.com <w6n7fIiP8gtU>
2014-12-10 12:25:27 -0500

    Add target back

--- models/thermometer.js
+++ models/thermometer.js
@@ -1,10 +1,17 @@
 var sensor = require('ds18x20');
+var fs = require('fs');
 
 /*
  * Get the Thermometer Object.
  */
 exports.getByDeviceId = function(deviceId) {
     console.log("Reading device: " + deviceId);
-    return sensor.get(deviceId);
+    if (deviceId.indexOf("/") < 0) {
+        return sensor.get(deviceId);
+    } else {
+        // must be a file
+        return fs.readFileSync(deviceId, 'utf8');
+    }
+
 };
 
