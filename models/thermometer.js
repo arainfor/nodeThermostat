a2e86bf96084837c361cff0b2fdeb45ca427a00f
arainfor <w6n7fIiP8gtU>
2014-12-10 20:55:44 -0600

    Cleanup conversions

--- models/thermometer.js
+++ models/thermometer.js
@@ -10,12 +10,12 @@ exports.getByDeviceId = function(units, deviceId) {
         if (units === 'c') {
           return sensor.get(deviceId);
         } else {
-          return cToF(sensor.get(deviceId));
+          return this.cToF(sensor.get(deviceId));
         }
     } else {
         // must be a file
         if (units === 'c') {
-          return fToC(fs.readFileSync(deviceId, 'utf8').trim());
+          return this.fToC(fs.readFileSync(deviceId, 'utf8').trim());
         } else {
           return fs.readFileSync(deviceId, 'utf8').trim();
 
