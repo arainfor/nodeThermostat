3fc52402bb4f075cf64207a5782ae9978d868a44
arainfor@gmail.com <w6n7fIiP8gtU>
2014-12-22 16:16:22 -0500

    Add mock data

---
+++ mock/relay_mock.js
@@ -0,0 +1,75 @@
+var _ = require('underscore');
+
+var relays = [
+  {"id":0,"name":"System On","path":"/var/thermronstat/status/0","value":"0.0"},
+  {"id":1,"name":"Running","path":"/var/thermronstat/relay/0","value":"0.0"}
+];
+
+var id = relays.length+1;
+
+exports.list = function (req, res) {
+  res.end(JSON.stringify(relays));
+};
+
+exports.create = function (req, res) {
+  var relay = {
+    id: id++,
+    name: req.body.name,
+    path: req.body.path,
+  };
+  relays.push(relay);
+  res.end(JSON.stringify(relay));
+};
+
+exports.update = function (req, res) {
+  var relay = _.findWhere(relays, {id: parseInt(req.params.id)});
+    if (relay) {
+    if (req.body.name) {
+      relay.name = req.body.name;
+    }
+    if (req.body.path) {
+      relay.path = req.body.path;
+    }
+  } else {
+    res.send('Relay with id = ' + req.params.id + ' does not exists', 500);
+  }
+  res.end(JSON.stringify(relay));
+};
+
+exports.query = function (req, res) {
+
+  if (req.session.relays) {
+    relays = req.session.relays;
+  }
+
+  var property = req.params.property;
+  var value = req.params.value;
+  var result = _.filter(relays, function (relay) {
+    return relay[property] == value;
+  });
+
+  res.end(JSON.stringify(result || []));
+};
+
+exports.get = function (req, res) {
+
+  var property = 'id';
+  var value = req.params.id;
+  var result = _.filter(relays, function (relay) {
+    return relay[property] == value;
+  });
+
+  res.end(JSON.stringify(_.first(result) || {}));
+};
+
+exports.delete = function (req, res) {
+  var id = req.params.id;
+  relays = _.reject(relays, function (relay) {
+    return relay.id == id;
+  });
+  res.end(JSON.stringify(relays || []));
+};
+
+exports.error = function (req, res) {
+  res.send('Error while getting relays', 500);
+};
---
+++ mock/temperatures_mock.js
@@ -0,0 +1,76 @@
+//{"temperatures":[{"id":0,"name":"indoor","source":"28-0000065ce60f","centigrade":30.9},{"id":1,"name":"target","source":"/var/thermronstat/target/0","centigrade":"60.0"},{"id":2,"name":"outdoor","source":"28-0000065ce60f","centigrade":30.9},{"id":3,"name":"plenum","source":"28-0000065ce60f","centigrade":30.9}]}
+var _ = require('underscore');
+
+var temperatures = [
+{"id":0,"name":"indoor","source":"28-0000065ce60f","centigrade":30.9},
+{"id":1,"name":"target","source":"/var/thermronstat/target/0","centigrade":"60.0"},
+{"id":2,"name":"outdoor","source":"28-0000065ce60f","centigrade":30.9},
+{"id":3,"name":"plenum","source":"28-0000065ce60f","centigrade":30.9}
+]
+
+exports.list = function (req, res) {
+  res.end(JSON.stringify(temperatures));
+};
+
+exports.create = function (req, res) {
+  var temperature = {
+    id: id++,
+    name: req.body.name,
+    path: req.body.path,
+  };
+  temperatures.push(temperature);
+  res.end(JSON.stringify(temperature));
+};
+
+exports.update = function (req, res) {
+  var temperature = _.findWhere(temperatures, {id: parseInt(req.params.id)});
+    if (temperature) {
+    if (req.body.name) {
+      temperature.name = req.body.name;
+    }
+    if (req.body.path) {
+      temperature.path = req.body.path;
+    }
+  } else {
+    res.send('Temperature with id = ' + req.params.id + ' does not exists', 500);
+  }
+  res.end(JSON.stringify(temperature));
+};
+
+exports.query = function (req, res) {
+
+  if (req.session.temperatures) {
+    temperatures = req.session.temperatures;
+  }
+
+  var property = req.params.property;
+  var value = req.params.value;
+  var result = _.filter(temperatures, function (temperature) {
+    return temperature[property] == value;
+  });
+
+  res.end(JSON.stringify(result || []));
+};
+
+exports.get = function (req, res) {
+
+  var property = 'id';
+  var value = req.params.id;
+  var result = _.filter(temperatures, function (temperature) {
+    return temperature[property] == value;
+  });
+
+  res.end(JSON.stringify(_.first(result) || {}));
+};
+
+exports.delete = function (req, res) {
+  var id = req.params.id;
+  temperatures = _.reject(temperatures, function (temperature) {
+    return temperature.id == id;
+  });
+  res.end(JSON.stringify(temperatures || []));
+};
+
+exports.error = function (req, res) {
+  res.send('Error while getting temperatures', 500);
+};
